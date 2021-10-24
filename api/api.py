bbff3ccce860eca3929010bd15d6734593a91544
Nikhil Kandkur <nkandkur@berkeley.edu>
2021-10-24 02:34:13 -0700

    add changes

--- api/api.py
+++ api/api.py
@@ -2,7 +2,7 @@ from psycopg2.errors import SerializationFailure
 from flask import jsonify
 from flask import Flask
 from flask import request
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 import sys
 import time
@@ -11,6 +11,7 @@ import populartimes
 import psycopg2
 import urllib
 
+import geopy
 import sched
 s = sched.scheduler(time.time, time.sleep)
 
@@ -51,19 +52,25 @@ def fulfil_request(data):
     We will create a helper method to check whether the data of interest is in CockroachDB and instead use that if possible
     (an if statement of sorts)
     """
+    global cache
+    
+    sorted_values = cache
 
-    ## if data in cockroachdb, get that and return
-    #cockroach_data, http_code = fulfill_request_helper("SELECT * FROM ____", conn)
-    for library in libraries:
-        data = populartimes.get_id(api_key, library)
-        print(libraries[library], data.get("current_popularity"))
-        cockroach_data, http_code = fulfill_request_helper("INSERT INTO location_data ${} VALUES ${}", conn)
-    if http_code != 404:
-        return cockroach_data
-    else:
-        ## else get the data from the google maps api and return that
-        ## make sure to add this data to cockroach db too!
-        return None
+    if "radius" and "coordinates" in data:
+        temp = []
+        # sorted_values = sorted([cache[place_id] for place_id in cache], key: lambda x: (geopy.distance.distance(x['loc_point'], x['popularity']))
+        for x in sorted_values:
+            # x = sorted_values[place_id]
+            # place_id = x['id']
+            location = (float(val.strip("()")) for val in x['loc_point'].split(","))
+            if geopy.distance.distance(location, data["coordinates"]).miles < data['radius']:
+                temp.append(x)
+        sorted_values = sorted([x for x in temp], 
+                        key= lambda x: (geopy.distance.distance(location, data["coordinates"]).miles, x['popularity']))
+                   
+    return {'ret': sorted_values}
+
+    
 
 ## get all info 
 @app.route(f'/api/v0/get_all', methods = ["GET"])
@@ -72,43 +79,75 @@ def add_queue():
     print(api_key)
     if check_headers(data, ["type"]):
         ## do thing
-        fulfil_request(data)
-        return "",200
+        return fulfil_request(data),200
+    return "bad request", 404
 
 #A json get request with {coordinates:(lat,lng), radius:float, type: library, building, all}
 @app.route(f'/api/v0/get_near', methods = ["GET"])
 def remove_queue():
     data = request.get_json()
-    if check_headers(data, ["coordinates", "radius", "type", "building", "all"]):
-        return data, 200
+    if check_headers(data, ["coordinates", "radius","type"]):
+        return fulfil_request(data),200
         #return the final data
+    return "bad request", 404
 
 
 @app.route(f'/api/v0/alive', methods = ["GET"])
 def ping_api():
     return "I'm There!", 200
 
-def back_run(conn):
+def get_key_time(key, cur):
+    print(key)
+    cur.execute("SELECT * FROM location_data WHERE id LIKE %s", (key,))
+    return cur.fetchone()
+
+def back_run(conn, time_diff = timedelta(minutes = 10)):
+    global cache
+
+    # with open("test.json","r") as f:
+    #     temp = json.loads(f.read())
+    #     for x in temp:
+    #         try:
+    #             temp[x]['last_time_updated'] = datetime.datetime.strptime(temp[x]['last_time_updated'], "%Y-%m-%d %H:%M:%S")
+    #         except:
+    #             continue
+    #     cache = temp
+    #     # print(cache)
+    # return
+
     libraries = read_libraries()
     columns = ['id', 'type', 'popularity', 'name', 'loc_point', 'last_time_updated', 'url', 'popular_times']
-    temp_cache = []
+    temp_cache = {}
     for library in libraries:
         with conn.cursor() as cur:
             data = populartimes.get_id(api_key, library)
-            
-            sql_statement = f'INSERT INTO location_data ({", ".join(columns)}) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
-            variables =  (library, "library", data.get("current_popularity",0), libraries.get(library), 
-                f'({data.get("coordinates").get("lat")},{data.get("coordinates").get("lng")})', datetime.utcnow(), 
-                f'https://www.google.com/maps/place/?q=place_id:{library}', json.dumps(data.get("populartimes", [])))
-            cur.execute(sql_statement, variables)
-            # print(f'delete_accounts(): status message: {cur.statusmessage}')
-            library_cache = {}
-            for k,v in zip(columns, variables):
-                library_cache[k] = v
-            print(library_cache)
-            temp_cache.append(library_cache)
-    cache = temp_cache
+            db_data = get_key_time(library, cur)
+            if db_data:
+                updated_time = db_data[5]
+            if not db_data or datetime.utcnow() - updated_time >= time_diff:
+                print('hi')
+                if db_data:
+                    print(datetime.utcnow() - updated_time)
+                print(data)
+                sql_statement = f'INSERT INTO location_data ({", ".join(columns)}) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
+                data =  (library, "library", data.get("current_popularity",0), libraries.get(library), 
+                    f'({data.get("coordinates").get("lat")},{data.get("coordinates").get("lng")})', datetime.utcnow(), 
+                    f'https://www.google.com/maps/place/?q=place_id:{library}', json.dumps(data.get("populartimes", [])))
+                cur.execute(sql_statement, data)
+                # print(f'delete_accounts(): status message: {cur.statusmessage}')
+            if data:
+                library_cache = {}
+                for k,v in zip(columns, data):
+                    library_cache[k] = v
+                print(library_cache)
+                temp_cache[library] = library_cache
+    cache.update(temp_cache)
+    cache = sorted([cache[place_id] for place_id in cache], key=  lambda x: x['popularity'])
+    # with open("test.json","w") as f:
+    #     f.write(json.dumps(cache, default=str))
     conn.commit()
+    s.enter(60, 1, back_run, (conn,))
+
 
 if __name__ =='__main__':
     # ref.on_snapshot(lambda x,y,z: firestore_update(x,y,z,datas))
@@ -125,8 +164,12 @@ if __name__ =='__main__':
 
             #cockroach_data, http_code = fulfill_request_helper(sql_statement, conn)
 
-    
-    back_run(conn)
+    print('hello2')
+    s.enter(0, 1, back_run, (conn,))
+    s.run()
+    print('hello')
+    # back_run(conn)
+
     app.run(host="0.0.0.0")
     # def fulfill_request_helper(sql_statement, conn):
 
