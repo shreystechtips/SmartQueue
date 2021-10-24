5cd993505c78a118034582c757a3dbefa4f74a11
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-10-24 03:12:58 -0700

    Complete base working frontend

--- smartqueue-app/src/Components/Library.js
+++ smartqueue-app/src/Components/Library.js
@@ -1,9 +1,68 @@
-import React from 'react'
+import {useState} from 'react'
 
-export default function Library() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons'
+
+const busyThreshold = 20;
+
+const getHourRange = hours => {
+    let ranges = []
+    let inRange = false
+    let currentRange = []
+    for (let i = 0; i < hours.length; i++) {
+        if (hours[i] > busyThreshold && !inRange) {
+            inRange = true;
+            currentRange.push(i + 1)
+        } else if (hours[i] < busyThreshold && inRange) {
+            inRange = false;
+            currentRange.push(i)
+            ranges.push(currentRange)
+            currentRange = []
+        }
+    }
+    if (currentRange.length === 1) {
+        currentRange.push(24);
+        ranges.push(currentRange);
+    }
+    return ranges
+}
+
+const hourToString = hour => {
+    if (hour <= 12) {
+        return String(hour) + ":00 AM"
+    } else {
+        return String(hour - 12) + ":00 PM"
+    }
+}
+
+export default function Library({info}) {
+    const [expanded, setExpanded] = useState(false);
+    const toggleExpanded = () => setExpanded(!expanded);
+
+    let day = new Date().getDay()
+    let shifted = day === 0 ? 6 : day - 1
+    let hourRanges = getHourRange(info.popular_times[shifted].data)
+
+    let expandInfo = (
+        <div className="library-extra">
+            <div className="library-times">
+                <div className="library-times-title">Busy Times:</div>
+            <div className="library-times-ranges">{hourRanges.map(range =>
+            hourToString(range[0]) + " - " + hourToString(range[1]))}
+            </div>
+            </div>
+            <a className="library-location" href={info.url} target="_blank" rel="noreferrer">
+                Location
+            </a>
+        </div>
+    )
     return (
-        <div id="library">
-            
+        <div className="library" onClick={toggleExpanded}>
+            <div className="library-name">{info.Name}</div>
+            <div className="library-popularity">Busyness Level: {info.current_popularity}%
+            </div>
+            <FontAwesomeIcon icon={expanded ? faCaretUp : faCaretDown}className="library-drop" />
+            {expanded ? expandInfo : <span />}
         </div>
     )
 }
--- smartqueue-app/src/Components/Title.js
+++ smartqueue-app/src/Components/Title.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import {React, useState} from 'react'
+
+const descriptions = [
+    "The library is ✨open✨",
+    "So you don't have to sit on the library floor",
+    "Convenient libraries, for your convenience"
+]
 
 export default function Title() {
+    const [descIndex, setDescIndex] = useState(Math.floor(Math.random() * descriptions.length))
+    const toggleDescIndex = () => setDescIndex((descIndex + 1) % 3)
     return (
-        <div id="title">
-            <div id= "title-header">
+        <div id="title" onClick={toggleDescIndex}>
+            <div id="title-header">
                 Cal Study Spaces
             </div>
 
-            <div id= "title-description">
-                The library is ✨open✨
+            <div id="title-description">
+                {/* The library is ✨open✨ */}
+                {descriptions[descIndex]}
             </div>
         </div>
 
